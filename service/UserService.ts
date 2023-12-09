94382e4a5df121483d72c0e5e2e8a160490dfd3c
Arshie13 <aaron.ciervo-20@cpu.edu.ph>
2023-12-09 17:47:45 +0800

    little fixes

--- service/UserService.ts
+++ service/UserService.ts
@@ -1,62 +1,72 @@
-  //note: error handling should be in here.
+//note: error handling should be in here.
 
 import UserController from '../controller/UserController';
 import UserDAO from '../dao/UserDAO';
 import express from 'express';
-  export interface IPersonData {
-    first_name: string,
-    last_name: string,
-    email: string,
-    password: string,
-    phone_number: string,
-    age: number,
-    user_type: string
+
+interface IPersonData {
+  id: number,
+  first_name: string,
+  last_name: string,
+  email: string,
+  password: string,
+  phone_number: string,
+  age: number,
+  user_type: string
+}
+
+interface ICreateUserData {
+  first_name: string,
+  last_name: string,
+  email: string,
+  password: string,
+  phone_number: string,
+  age: number,
+  user_type: string
+}
+
+class UserService {
+  async createUser(userData: ICreateUserData): Promise<number> {
+    const {
+      first_name, last_name, email, password, phone_number, age, user_type
+    } = userData;
+    return UserDAO.createUser(
+      first_name, last_name, email, password, phone_number, age, user_type
+    );
   }
 
-  class UserService {
-    async createUser(IPersonData: IPersonData): Promise<number> {
-      const { 
-        first_name, last_name, email, password, phone_number, age, user_type 
-      } = IPersonData;
-      return UserDAO.createUser(
-        first_name, last_name, email, password, phone_number, age, user_type
-        );
-    }
-
-    async getUserbyId(id: number) {
-      return UserDAO.getUser(id);
-    }
-
-    async getUserByEmail(email: string) {
-      return UserDAO.getUserByEmail(email);
-    }
-    
-    async updateUser(personData: IPersonData) {
-      const { 
-        id, first_name, last_name, email, password, phone_number, age, user_type
-      } = personData;
-      return UserDAO.updateUser(
-        id, first_name, last_name, email, password, phone_number, age, user_type
-      );
-    }
-
-    async deleteUser(id: number) {
-      return UserDAO.deleteUser(id);
-    }
-
-    async logInUser(req: express.Request, res: express.Response){
-      const { email, password } = req.body;
-      try{
-        const foundUser = await UserController.loginUser(req, res);
-        res.status(200).send(foundUser);
-      } catch(error){
-        console.error('Login Error:', error);
-        res.status(500).json({ message: 'Internal server error' });
-      }
-    }
-
+  async getUserbyId(id: number) {
+    return UserDAO.getUser(id);
   }
-  export default new UserService();
 
-  
-  
+  async getUserByEmail(email: string) {
+    return UserDAO.getUserByEmail(email);
+  }
+
+  async updateUser(personData: IPersonData) {
+    const {
+      id, first_name, last_name, email, password, phone_number, age, user_type
+    } = personData;
+    return UserDAO.updateUser(
+      id, first_name, last_name, email, password, phone_number, age, user_type
+    );
+  }
+
+  async deleteUser(id: number) {
+    return UserDAO.deleteUser(id);
+  }
+
+  async logInUser(req: express.Request, res: express.Response) {
+    const { email, password } = req.body;
+    try {
+      const foundUser = await UserController.loginUser(req, res);
+      res.status(200).send(foundUser);
+    } catch (error) {
+      console.error('Login Error:', error);
+      res.status(500).json({ message: 'Internal server error' });
+    }
+  }
+}
+export default new UserService();
+
+
