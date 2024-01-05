db7f6b6550f898f30cd55c4b35897da862359ca6
jnwync <jonwayne.cabusbusan@gmail.com>
2024-01-06 07:06:38 +0800

    multer image upload finished

--- middleware/multer.ts
+++ middleware/multer.ts
@@ -7,11 +7,14 @@ const storage = multer.diskStorage({
     cb(null, path.join(__dirname, '../uploaded-images'));
   },
   filename: function (req, file, cb) {
-    const uniqueFilename = generateUniqueFilename(file.originalname);
+    const userEmail = req.query.userEmail as string;
+
+    const uniqueFilename = generateUniqueFilename(file.originalname, userEmail);
+
     cb(null, file.fieldname + '-' + uniqueFilename + path.extname(file.originalname));
   },
 });
 
 const upload = multer({ storage: storage });
 
-export default upload
+export default upload;
--- routes/images/images.ts
+++ routes/images/images.ts
@@ -8,27 +8,30 @@ export const imageRouter = express.Router();
 
 const imageController = new ImageController();
 
-imageRouter.post('/upload-image', upload.single('image'), async(req: Request, res: Response) => {
-    console.log(req.file)
-    if (!req.file) {
-        return res.status(400).send('No file uploaded.');
-      }
+imageRouter.post('/upload-image', upload.single('image'), async (req: Request, res: Response) => {
+  console.log(req.file);
+  
+  const userEmail = req.query.userEmail;
 
-      const filePath = path.join(__dirname, '../uploaded-images', req.file.filename) ;
-      try {
-        const insertedFile = await db('files').insert({
-          filename: req.file.filename,
-          originalname: 'palagpat',
-          user_id: 1,
-          path: filePath,
-        }).returning('*');
-    
-        return res.json({
-          message: 'File uploaded successfully.',
-          file: insertedFile[0],
-        });
-      } catch (error) {
-        console.log(error)
-        return res.status(500).json({ error: 'Error uploading file to the database.' });
-      }
+  if (!req.file) {
+    return res.status(400).send('No file uploaded.');
+  }
+
+  const filePath = path.join(__dirname, '../uploaded-images', req.file.filename);
+  try {
+    const insertedFile = await db('files').insert({
+      filename: req.file.filename,
+      originalname: req.file.filename,
+      user_email: userEmail,
+      path: filePath,
+    }).returning('*');
+
+    return res.json({
+      message: 'File uploaded successfully.',
+      file: insertedFile[0],
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ error: 'Error uploading file to the database.' });
+  }
 });
